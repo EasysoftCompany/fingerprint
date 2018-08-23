233921fb30ff249c3e2e37420409b977af1ea7ae
Gerardo Rico <EasysoftCompany@users.noreply.github.com>
2018-08-23 17:22:00 -0500

    BD Class
    implementacion de una clase gestora de conexiones a base de datos

---
+++ src/BD/cDatos.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2014 Easysoft Company
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package BD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Ger
+ */
+public class cDatos {
+    
+    private String usrBD;
+    private String passBD;
+    private String urlBD;
+    private String driverClassName;
+    private Connection conn = null;
+    private Statement estancia;
+ 
+    public cDatos(String usuarioBD, String passwordBD, String url, String driverClassName) {
+        this.usrBD = usuarioBD;
+        this.passBD = passwordBD;
+        this.urlBD = url;
+        this.driverClassName = driverClassName;
+    }
+    public cDatos() {
+        //poner los datos apropiados
+        this.usrBD = "root";
+        //this.passBD = "34syS0ft"; // CASA
+        this.passBD = "Ali4nz@01"; // Produccion
+        this.urlBD = "jdbc:mysql://127.0.0.1:3306/SITE";//Si Somos huevones le cambiamos la BD xD
+        this.driverClassName = "com.mysql.jdbc.Driver";
+    }
+    
+    //metodos para establecer los valores de conexion a la BD
+    public void setUsuarioBD(String usuario) throws SQLException {
+        this.usrBD = usuario;
+    }
+    public void setPassBD(String pass) {
+        this.passBD = pass;
+    } 
+    public void setUrlBD(String url) {
+        this.urlBD = url;
+    }
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+    
+    //Conexion a la BD
+    public void conectar() throws SQLException {
+        try {
+            Class.forName(this.driverClassName).newInstance();
+            this.conn = DriverManager.getConnection(this.urlBD, this.usrBD, this.passBD);
+ 
+        } catch (Exception err) {
+            System.out.println("Error " + err.getMessage());
+        }
+    }
+    
+    //Cerrar la conexion de BD
+    public void cierraConexion() throws SQLException {
+        this.conn.close();
+    }
+    
+    //Metodos para ejecutar sentencias SQL
+    public ResultSet consulta(String consulta) throws SQLException {
+        this.estancia = (Statement) conn.createStatement();
+        return this.estancia.executeQuery(consulta);
+    } 
+    public void actualizar(String actualiza) throws SQLException {
+        this.estancia = (Statement) conn.createStatement();
+        estancia.executeUpdate(actualiza);
+    } 
+    public ResultSet borrar(String borra) throws SQLException {
+        Statement st = (Statement) this.conn.createStatement();
+        return (ResultSet) st.executeQuery(borra);
+    } 
+    public int insertar(String inserta) throws SQLException {
+        Statement st = (Statement) this.conn.createStatement();
+        return st.executeUpdate(inserta);
+    }
+}
